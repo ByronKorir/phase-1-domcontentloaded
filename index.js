f52a9f1386f1a0e93bc19b21777b01a1f654928c
byron korir <byron.korir@student.moringaschool.com>
2023-09-29 21:14:44 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+  document.addEventListener('DOMContentLoaded', () =>{
+   const p =document.getElementById('text');
+    console.log(p)
+    p.textContent ='This is really cool!'
+  })
